bca6bda79f62203c118e674d1ef8f3c7271a4d90
Marcus La Bar <mlabar26@byu.edu>
2023-09-12 17:15:47 -0600

    Marcus' Work So Far

--- Wordle.py
+++ Wordle.py
@@ -10,15 +10,28 @@ import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
-def wordle():
+# Selects random word
+randomWord = random.choice(FIVE_LETTER_WORDS)
+nextLetter = []
 
+# For previous code tries, if needed
+# ([*randomWord])
+
+
+def wordle():
     def enter_action(s):
         gw.show_message("You have to implement this method.")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-# Startup code
+    # Breaks word up
+    for letter in randomWord:
+        nextLetter.append(letter)
+        gw.set_square_letter(0, 0, letter)
+        print(nextLetter)
 
+
+# Startup code
 if __name__ == "__main__":
     wordle()
